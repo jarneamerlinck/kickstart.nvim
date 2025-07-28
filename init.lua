785cf079324567080bac8610266f3ee3dd611636
jarneamerlinck <39766278+jarneamerlinck@users.noreply.github.com>
2025-07-28 12:42:06 +0000

    exec command for todo's

--- init.lua
+++ init.lua
@@ -465,7 +465,7 @@ require('nixCatsUtils.lazyCat').setup(pluginList, nixLazyPath, {
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>st', ':TodoTelescope', { desc = '[S]earch [T]odo' })
+      vim.keymap.set('n', '<leader>st', ':TodoTelescope<CR>', { desc = '[S]earch [T]odo' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
