15a76a6fae54a13e0fab16b540af4cf8a1f3badb
jarneamerlinck <jarneamerlinck@pm.me>
2025-08-27 19:13:52 +0200

    used 25.05 instead of unstable

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
   description = "A personal neovim config started from nixcats kickstarter.nvim";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixCats.url = "github:BirdeeHub/nixCats-nvim";
 
     # neovim-nightly-overlay = {
