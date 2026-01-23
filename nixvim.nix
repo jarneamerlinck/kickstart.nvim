1ca6bc1ca6910b2b015032d949d4102678118244
jarneamerlinck <jarneamerlinck@pm.me>
2026-01-23 22:42:06 +0100

    cleaning up

--- nixvim.nix
+++ nixvim.nix
@@ -114,27 +114,6 @@ in
     P.S. You can delete this when you're done too. It's your config now! :)
   */
 
-  # You can easily change to a different colorscheme.
-  # Add your colorscheme here and enable it.
-  # Don't forget to disable the colorschemes you arent using
-  #
-  # If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  # colorschemes = {
-  #   # https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
-  #   tokyonight = {
-  #     enable = true;
-  #     settings = {
-  #       # Like many other themes, this one has different styles, and you could load
-  #       # any other, such as 'storm', 'moon', or 'day'.
-  #       style = "night";
-  #       styles = {
-  #         comments = {
-  #           italic = false; # Disable italics in comments
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
   # Allow stylix for theming
   colorschemes =
     if hasStylix then
@@ -145,6 +124,7 @@ in
     else
       {
         # Fallback colorscheme when Stylix is not installed
+        # https://nix-community.github.io/nixvim/colorschemes/tokyonight/index.html
         tokyonight = {
           enable = true;
           settings = {
