bc2818377f8297be67013f003726f1de78090c4d
jarneamerlinck <jarneamerlinck@pm.me>
2026-01-23 23:32:06 +0100

    Revert "added devicons"
    This reverts commit a390b4b982a30496ae60eb3f9745bd02186ba604.

--- config/plugins/custom/devicons.nix
+++
@@ -1,6 +0,0 @@
-{
-  # https://nix-community.github.io/nixvim/plugins/web-devicons/index.html
-  plugins.web-devicons = {
-    enable = true;
-  };
-}
--- nixvim.nix
+++ nixvim.nix
@@ -38,7 +38,6 @@ in
     ./config/plugins/custom/alpha.nix
     ./config/plugins/custom/auto-save.nix
     ./config/plugins/custom/floaterm.nix
-    ./config/plugins/custom/devicons.nix
   ];
 
   /*
