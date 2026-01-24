78690b2a8e88adbc60b4a5845e27d9d2117ef3c7
jarneamerlinck <jarneamerlinck@pm.me>
2026-01-24 12:12:38 +0100

    added default file ignore patterns

--- config/plugins/kickstart/telescope.nix
+++ config/plugins/kickstart/telescope.nix
@@ -134,6 +134,17 @@
     };
     settings = {
       extensions.__raw = "{ ['ui-select'] = { require('telescope.themes').get_dropdown() } }";
+      defaults = {
+        file_ignore_patterns = [
+          ".git/"
+          "^node_modules/"
+          "^vendor/"
+          "%.jpg"
+          "%.png"
+          "%.tox/"
+          "%.oh-my-zsh/"
+        ];
+      };
     };
   };
 
