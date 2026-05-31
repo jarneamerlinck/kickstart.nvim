ecaff9a948542b58046d8b752fa56068b2f725bd
jarneamerlinck <jarneamerlinck@pm.me>
2026-05-31 15:01:46 +0200

    feat: disable devenv features not wanted

--- devenv.nix
+++ devenv.nix
@@ -8,10 +8,10 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  # env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  # packages = [ pkgs.git ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
@@ -23,15 +23,15 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
 
   # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
+  # enterShell = ''
+  #   hello         # Run scripts directly
+  #   git --version # Use packages
+  # '';
 
   # https://devenv.sh/tasks/
   # tasks = {
